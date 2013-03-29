9a7d986af85f1aed1d036608c2368945c09051e5
Ryutaro YOSHIBA <ryuzee@gmail.com>
2013-03-30 08:06:10 +0900

    updated for vagrant 1.1.

--- lib/sahara.rb
+++ lib/sahara.rb
@@ -1,1 +1,19 @@
-require 'vagrant'
+# -*- encoding: utf-8 -*-
+# vim: set fileencoding=utf-8
+
+require "vagrant"
+require File.expand_path("../sahara/session", __FILE__)
+
+module Sahara 
+  class Plugin < Vagrant.plugin("2")
+    name "sahara"
+    description <<-DESC
+    Sahara
+    DESC
+
+    command("sandbox") do
+      require File.expand_path("../sahara/command/root", __FILE__)
+      Command::Root
+    end
+  end
+end
--- lib/sahara/command/vagrant.rb
+++
@@ -1,5 +0,0 @@
-require 'sahara'
-require 'sahara/session'
-require 'sahara/command/sandbox'
-
-Vagrant.commands.register(:sandbox) { Sahara::Command::Sandbox }
